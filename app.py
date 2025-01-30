872febe6ba68ec15baad3ab59b5470fa31be7ec3
Adarsh Gupta <blueadarsh1@gmail.com>
2025-01-30 22:04:30 +0530

    update serverl link

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ UPLOAD_FOLDER = os.path.join(basedir, 'static', 'uploads')
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
-app.config['SERVER_NAME'] = 'my-portfolio-website-hbsc.onrender.com'  # Replace with your actual domain
+app.config['SERVER_NAME'] = 'adarshgpt.onrender.com'  # Replace with your actual domain
 app.config['PREFERRED_URL_SCHEME'] = 'https'  # Or 'http' if you're not using HTTPS
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB max-limit
